c0080346031a344e906d5183b636f1610af1d629
Leonardo <josue.aguilar1995@gmail.com>
2022-05-31 16:54:35 -0500

    connected server to election database

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require("express");
 const res = require("express/lib/response");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
 
 // express middleware:
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to mysql database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "MySQLpass123!!",
+    database: "election",
+  },
+  console.log("Connected to the election database")
+);
 
 // default response for request not found
 app.use((req, res) => {
